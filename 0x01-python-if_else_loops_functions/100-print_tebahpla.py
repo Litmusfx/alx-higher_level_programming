d63afe5fe858fb6aada014fb6d70b68158e16ebd
litmusfx <johnsonwk302@gmail.com>
2022-12-20 16:04:48 -0500

    100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
 for i in range(122, 96, -1):
-    if (i % 2 != 0):
-        print('{}'.format(chr(i - 32)), end='')
-    else:
-        print('{}'.format(chr(i)), end='')
+    print("{}".format(chr(i - 32) if i % 2 else chr(i)), end="")
