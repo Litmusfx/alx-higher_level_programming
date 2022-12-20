d55aff4ac64a2a9266e5a1bb2dd1072bf2f82ee9
litmusfx <johnsonwk302@gmail.com>
2022-12-20 14:56:13 -0500

    3-print

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -3,4 +3,4 @@
 Print the ASCII alphabet in lowercase, not followed by a new line.
 """
 for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")[2~+
+    print("{}".format(chr(letter)), end="")
